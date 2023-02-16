8c4ff70003b4a0a48d04cc85649b91208e381e5c
IndigoTar <egorgius34@gmail.com>
2023-02-16 18:10:54 +0700

    gotovo
    gotovo2.0

--- Assets/Scripts/PersControl.cs
+++ Assets/Scripts/PersControl.cs
@@ -5,26 +5,15 @@ using UnityEngine;
 public class PersControl : MonoBehaviour
 {
     public float speed = 1f;
-    
+  
     public Joystick joystick;
-    
-    Rigidbody rb;
 
-   
-
-
-    private void Start()
-    {
-        
-    }
-
+  
     private void Update()
     {
         float movement = joystick.Horizontal;
         transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime;
-        
-       
-    }
 
+    }
     
 }
