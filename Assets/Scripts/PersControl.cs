e50cedd6addaa5bae55d443de5b2a515f3517a9a
MaksimTam <eilarend228@gmail.com>
2023-02-17 16:57:19 +0700

    new
    q

--- Assets/Scripts/PersControl.cs
+++ Assets/Scripts/PersControl.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class PersControl : MonoBehaviour
 {
     public float speed = 1f;
+    public float jump = 1f;
+    private Rigidbody rigidbody;
+
   
     public Joystick joystick;
 
-  
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
+        float verticalMove = joystick.Vertical;
+        transform.position += new Vector3(verticalMove, 0) * jump * Time.deltaTime;
+
         float movement = joystick.Horizontal;
         transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime;
 
